1ba595bb168e8c4cabb9e8900f898a33a7285a41
tonywang806 <tonywang806@gmail.com>
2017-04-11 19:26:24 +0900

    [change]担当者設定画面と稼働環境設定画面追加
    ・稼働環境設定画面がPanel化しました。
    ・画面レイアウトがJqueryでうまく設定できず、いくつか箇所がTableに変更
    ・作業日の曜日表示機能を改善

---
+++ ChiyodaMobileOffice/www/scripts/ButtonEventBind.js
@@ -0,0 +1,58 @@
+﻿function BtnEventBind(){
+    //Event Handler Bind
+    $("#btnScheduleRead").bind('click', GetSchedule);
+    $("#txtWorkDate").bind('change', SetWeekChar);
+    $("#btnEdit").bind('click', SetEditMode);
+    $("#btnAppend").bind('click', GetSchedule);
+    $("#btnDelete").bind('click', DeleteSchedule);
+    $("#btnOk").bind('click', SetDisplayMode);
+    $("#btnLoadConfig").bind('click', LoadStaffConfig);
+    $("#btnAddStaff").bind('click', AddStaff);
+    $("#btnMeasureTime").bind('click', SetMeasureTime);
+
+    //戻るボタンイベント
+    $("#btnStaffReturn").bind("click", function () {
+        location.href = '#DailyReport';
+    });
+    $("#btnEviromentReturn").bind("click", function () {
+        location.href = '#DailyReport';
+    });
+    $("#btnResultReturn").bind("click", function () {
+        location.href = '#DailyReport';
+    });
+    $("#btnSatffSettingEntry").bind("click", function () {
+        //CurrentStaff
+        var i = 0;
+        $("#ConfigList").find("select").each(function (index, elem) {
+            if ($(elem).val()) {
+                CurrentStaff[i] = $(elem).val();
+                i += 1;
+            }
+        });
+
+        SetWorkTime();
+
+        location.href = '#DailyReport';
+    });
+
+    $("#btnStaffSettingCancel").bind("click", function () {
+        location.href = '#DailyReport';
+    });
+
+    //$("#btnEvironmentMeasureEntry").bind("click", function () {
+    //    location.href = '#DailyReport';
+    //});
+    //$("#btnEvironmentMeasureCancel").bind("click", function () {
+    //    location.href = '#DailyReport';
+    //});
+
+
+    //子画面へ進む
+    $("#btnStaffSetting").bind("click", function () {
+        location.href = '#StaffSetting';
+    });
+    //$("#btnWorkEnviromentAssessment").bind("click", function () {
+    //    //location.href = '#WorkEnviromentAssessment';
+    //    $("#/*WorkEnviromentAssessment*/").trigger("updatelayout");
+    //});
+}
--- ChiyodaMobileOffice/www/scripts/business.js
+++ ChiyodaMobileOffice/www/scripts/business.js
@@ -2,12 +2,18 @@
 //曜日の選択肢
 var youbi = new Array("日", "月", "火", "水", "木", "金", "土");
 
+//担当者の選択肢
+var staff = new Array("", "今村", "和田", "潮田", "鈴木");
+
+var CurrentStaff = new Array();
+
 var EDIT_MODE = {
     DISPLAY: 0,
     EDIT: 1
 };
 
 var currentMode;
+var staffCount = 3;
 
 //作業日にディフォルト値を設定
 function InitailizeSearchConditions() {
@@ -21,7 +27,11 @@ function InitailizeSearchConditions() {
     var todayWeekChar = "(" + youbi[you] + ")";
 
     $("#txtWorkDate").val(today).select();
+
+    var weekLab = "<label id=\"txtWeekChar\"></label>";
+    $("#txtWorkDate").parents("div").append(weekLab);
     $("#txtWeekChar").text(todayWeekChar);
+    $("#selectWorkType").val("off").slider("refresh");
 }
 
 function InitailizeDailyReportForm() {
@@ -29,14 +39,19 @@ function InitailizeDailyReportForm() {
     $("td:eq(0)", $("#detal-table tr")).hide();
     $("#divEdit").hide();
 
-    //Event Handler Bind
-    $("#btnScheduleRead").bind('click', GetSchedule);
-    $("#txtWorkDate").bind('change', SetWeekChar);
-    $("#btnEdit").bind('click', SetEditMode);
-    $("#btnAppend").bind('click', GetSchedule);
-    $("#btnDelete").bind('click', DeleteSchedule);
-    $("#btnOk").bind('click', SetDisplayMode);
+    BtnEventBind();
+
+    staff.forEach(function (elem, index) {
+        $('#selectCaptain').append($('<option>').html(elem));
+        $('#selectOperator1').append($('<option>').html(elem));
+        $('#selectOperator2').append($('<option>').html(elem));
+    });
+   
     currentMode = EDIT_MODE.DISPLAY;
+}
+
+function GotoResultEntry() {
+    location.href = '#ResultEntry';
 }
 
 //曜日表示
@@ -58,11 +73,12 @@ function SetWeekChar() {
 function GetSchedule() {
     var newRow = "";
     if (currentMode === EDIT_MODE.DISPLAY) {
-        newRow = "<tr><td style=\"display: none;\"><input type=\"radio\"/></td><td>2</td><td>3</td><td>4</td><td>5</td><td>1</td><td>2</td><td>3</td><td>4</td><td><button class=\"ui-btn ui-corner-all\">実</button></td></tr>";
+        newRow = "<tr draggable=\"true\"><td style=\"display: none;\"><input type=\"checkbox\"/></td><td>2</td><td>3</td><td>4</td><td>5</td><td>1</td><td>2</td><td>3</td><td>4</td><td><button class=\"ui-btn ui-corner-all\" onClick=\"GotoResultEntry();\">実</button></td></tr>";
     } else {
-        newRow = "<tr><td><input type=\"radio\"/></td><td>2</td><td>3</td><td>4</td><td>5</td><td>1</td><td>2</td><td>3</td><td>4</td><td style=\"display: none;\"><button class=\"ui-btn ui-corner-all\">実</button></td></tr>";
+        newRow = "<tr draggable=\"true\"><td><input type=\"checkbox\"/></td><td>2</td><td>3</td><td>4</td><td>5</td><td>1</td><td>2</td><td>3</td><td>4</td><td style=\"display: none;\"><button class=\"ui-btn ui-corner-all\" onClick=\"GotoResultEntry();\">実</button></td></tr>";
     }
-    $('#detal-table').append(newRow);
+
+    $(newRow).appendTo("#detal-table").enhanceWithin();
 }
 
 //編集モード設定
@@ -94,7 +110,62 @@ function SetDisplayMode() {
 
 //行削除
 function DeleteSchedule() {
-    $('#detal-tbody input:radio:checked').each(function (index, element) {
+    $('#detal-tbody input:checkbox:checked').each(function (index, element) {
         $(element).parents("tr").remove();
     });
+}
+
+function LoadStaffConfig() {
+    $("#selectCaptain").val(staff[1]).select().selectmenu('refresh');
+    $("#selectOperator1").val(staff[2]).select().selectmenu('refresh');
+}
+
+function AddStaff() {
+
+    var staffSelectBoxName = "selectOperator" + staffCount;
+
+    var newStaffConfig = "<li>";
+    newStaffConfig = newStaffConfig + "<label style=\"font-weight:bold\">機員" + staffCount + "</label>";
+    newStaffConfig = newStaffConfig+"<div class=\"ui-grid-b\">";
+    newStaffConfig = newStaffConfig+"<div class=\"ui-block-a\"><select id=\""+ staffSelectBoxName+"\"></select></div>";
+    newStaffConfig = newStaffConfig+"<div class=\"ui-block-b\"><input type=\"checkbox\" id=\"checkOperator" + staffCount + "Rest\" /><label for=\"checkOperator" + staffCount +"Rest\">休暇</label></div>";
+    newStaffConfig = newStaffConfig+"<div class=\"ui-block-c\"><input type=\"checkbox\" id=\"checkOperator" + staffCount + "HalfRest\" /><label for=\"checkOperator" + staffCount + "HalfRest\">半休</label></div>";
+    newStaffConfig = newStaffConfig + "</div></li >";
+
+    $(newStaffConfig).appendTo("#ConfigList").enhanceWithin();
+
+    staffCount += 1;
+
+    staff.forEach(function (elem, index) {
+        $("#"+staffSelectBoxName).append($('<option>').html(elem));
+    });
+}
+
+function SetWorkTime() {
+    $(CurrentStaff).each(function (index, elem) {
+
+    });
+}
+
+function SetMeasureTime() {
+    var now = new Date();
+    var currentTime = "";
+
+
+
+    if (now.getHours() < 10) {
+        currentTime = "0"+now.getHours();
+    } else {
+        currentTime = now.getHours();
+    }
+
+    if (now.getMinutes() < 10) {
+        currentTime = currentTime + ":0" + now.getMinutes();
+    } else {
+        currentTime = currentTime + ":" + now.getMinutes();
+    }
+    
+    //alert(currentTime);
+    $("#txtMeasureTime").val(currentTime);
+    //$("#txtMeasureTime").val("7:11");
 }
